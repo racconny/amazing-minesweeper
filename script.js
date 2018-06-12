11c9e2beffd97908172bfe2748243af2af59553a
Vitaliy Kreminskiy <vkrmk13@gmail.com>
2018-06-12 18:02:04 +0300

    added mine generation

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-renderField(5);
+$("body").on("contextmenu", false);
+var dimension = 10;
+var arr = generateArray(dimension);
+renderField(dimension);
+generateMines(dimension, 50);
 
+
+function generateArray(d){
+    var f = [];
+    for (var i = 0; i < (d + 2); i++){
+        f[i] = [];
+        for (var j = 0; j < (d + 1); j++){
+            f[i][j] = 0;
+        }
+    }
+    return f;
+}
+
+function generateMines(d, a){
+    var generated = 0;
+    while (generated < a){
+        var t1 = Math.floor((Math.random() * d) + 1);
+        var t2 = Math.floor((Math.random() * d) + 1)
+        if (arr[t1][t2] !== "X"){
+            arr[t1][t2] = "X";
+            generated++;
+            document.querySelector(".field")
+            .childNodes[t1 -1]
+            .childNodes[t2 - 1]
+            .querySelector(".cell")
+            .innerHTML = "<span class='fas fa-skull'></span>";
+        }
+    }
+    //Math.floor((Math.random() * 10) + 1);
+}
 
 function renderField(d){
 //rendering game field
@@ -21,10 +54,13 @@ function renderField(d){
             row.appendChild(cell);
         }
     }
- 
 }
 
 function makeMove(x, y){
-    console.info("Move made in " + x + " , " + y);
+    console.info("Move made in " + (x + 1) + "," + (y + 1));
+    arr[x + 1][y + 1] = "o";
+    console.table(arr);
 }
+
+
 
