ad921d6d89f0f587497d79b3d884fa80832a5d1f
Vitaliy Kreminskiy <vkrmk13@gmail.com>
2018-06-12 20:54:25 +0300

    Added generation of numbers around mines, coloring of It with assigned color

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ $("body").on("contextmenu", false);
 var dimension = 10;
 var arr = generateArray(dimension);
 renderField(dimension);
-generateMines(dimension, 50);
+generateMines(dimension, 20);
 
 
 function generateArray(d){
     var f = [];
     for (var i = 0; i < (d + 2); i++){
         f[i] = [];
-        for (var j = 0; j < (d + 1); j++){
+        for (var j = 0; j < (d + 2); j++){
             f[i][j] = 0;
         }
     }
@@ -21,7 +21,7 @@ function generateMines(d, a){
     while (generated < a){
         var t1 = Math.floor((Math.random() * d) + 1);
         var t2 = Math.floor((Math.random() * d) + 1)
-        if (arr[t1][t2] !== "X"){
+        if (arr[t1][t2] != "X"){
             arr[t1][t2] = "X";
             generated++;
             document.querySelector(".field")
@@ -29,9 +29,58 @@ function generateMines(d, a){
             .childNodes[t2 - 1]
             .querySelector(".cell")
             .innerHTML = "<span class='fas fa-skull'></span>";
+            renderNumbers(t1, t2);
         }
     }
     //Math.floor((Math.random() * 10) + 1);
+}
+
+function syncArray(){
+    for (var i = 0; i < dimension; i++){
+        for (var j = 0; j < dimension; j++){
+            if ((arr[i + 1][j + 1] !== "X")
+             && (arr[i + 1][j + 1] !== 0)){
+            document.querySelector(".field")
+            .childNodes[i]
+            .childNodes[j]
+            .querySelector(".cell")
+            .innerHTML = "<span style='color: " + designNumber(i + 1, j + 1) + "'>" + arr[i + 1][j + 1] + "</span>";
+            }
+        }
+    }
+}
+
+function designNumber(x, y){
+    var dict = ["", "grey", "green", "blue", "red", "orange", "yellow", "purple","pink", "black"];
+    return dict[arr[x][y]]
+}
+
+function renderNumbers(x, y){
+    if (arr[x - 1][y - 1] !== "X"){
+        arr[x - 1][y - 1]++;
+    }
+    if (arr[x + 1][y + 1] !== "X"){
+        arr[x + 1][y + 1]++;
+    }
+    if (arr[x + 1][y - 1] !== "X"){
+        arr[x + 1][y - 1]++;
+    }
+    if (arr[x - 1][y + 1] !== "X"){
+        arr[x - 1][y + 1]++;
+    }
+    if (arr[x][y + 1] !== "X"){
+        arr[x][y + 1]++;
+    }
+    if (arr[x][y - 1] !== "X"){
+        arr[x][y - 1]++;
+    }
+    if (arr[x + 1][y] !== "X"){
+        arr[x + 1][y]++;
+    }
+    if (arr[x - 1][y] !== "X"){
+        arr[x - 1][y]++;
+    }
+    syncArray();
 }
 
 function renderField(d){
@@ -58,7 +107,6 @@ function renderField(d){
 
 function makeMove(x, y){
     console.info("Move made in " + (x + 1) + "," + (y + 1));
-    arr[x + 1][y + 1] = "o";
     console.table(arr);
 }
 
