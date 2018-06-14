cb0cc45cd9c099dde98f0355edcad4afa55de604
Vitaliy Kreminskiy <vkrmk13@gmail.com>
2018-06-14 23:54:20 +0300

    Added possibility of putting flags into the field. I implemented It using second array, that's cool, but not the most efficint way. I have no Idea what I should do next.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $("body").on("contextmenu", false);
+
 var dimension = 10;
 var arr = generateArray(dimension);
-renderField(dimension);
-generateMines(dimension, 96);
+var view_arr = generateArray(dimension);
 
+renderField(dimension);
+generateMines(dimension, Math.floor((dimension*dimension)*0.3));
+console.log(Math.floor((dimension*dimension)*0.8) + " mine(s) generated succesfuly");
 
 function generateArray(d){
     var f = [];
@@ -24,11 +27,6 @@ function generateMines(d, a){
         if (arr[t1][t2] != "X"){
             arr[t1][t2] = "X";
             generated++;
-            document.querySelector(".field")
-            .childNodes[t1 -1]
-            .childNodes[t2 - 1]
-            .querySelector(".cell")
-            .innerHTML = "<span class='fas fa-skull'></span>";
             renderNumbers(t1, t2);
         }
     }
@@ -39,12 +37,19 @@ function syncArray(){
     for (var i = 0; i < dimension; i++){
         for (var j = 0; j < dimension; j++){
             if ((arr[i + 1][j + 1] !== "X")
-             && (arr[i + 1][j + 1] !== 0)){
-            document.querySelector(".field")
-            .childNodes[i]
-            .childNodes[j]
-            .querySelector(".cell")
-            .innerHTML = "<span style='color: " + designNumber(i + 1, j + 1) + "'>" + arr[i + 1][j + 1] + "</span>";
+                 && (arr[i + 1][j + 1] !== 0)){
+                    document.querySelector(".field")
+                    .childNodes[i]
+                    .childNodes[j]
+                    .querySelector(".cell")
+                    .innerHTML = "<span style='color: " + designNumber(i + 1, j + 1) + "'>" + arr[i + 1][j + 1] + "</span>";
+            }
+            if ((arr[i + 1][j + 1] === "X")){
+                    document.querySelector(".field")
+                    .childNodes[i]
+                    .childNodes[j]
+                    .querySelector(".cell")
+                    .innerHTML = "<span class='fas fa-skull'></span>";
             }
         }
     }
@@ -80,7 +85,7 @@ function renderNumbers(x, y){
     if (arr[x - 1][y] !== "X"){
         arr[x - 1][y]++;
     }
-    syncArray();
+    //syncArray();
 }
 
 function renderField(d){
@@ -99,6 +104,8 @@ function renderField(d){
             var btn = document.createElement("button");
             btn.classList.add("cell");
             btn.setAttribute("onclick","makeMove(" + i + "," + j + ")");
+            btn.setAttribute("oncontextmenu", "putFlag(" + i + "," + j + ")");
+            //btn.toggleClass("flagged");
             cell.appendChild(btn);
             row.appendChild(cell);
         }
@@ -106,9 +113,32 @@ function renderField(d){
 }
 
 function makeMove(x, y){
-    console.info("Move made in " + (x + 1) + "," + (y + 1));
-    console.table(arr);
+    console.log("Move made in " + (x + 1) + ", " + (y + 1));
+    if (arr[x][y] === "X"){
+        syncArray();
+        console.log("game over");
+    }
 }
 
+function putFlag(x, y){
+    var content = document.querySelector(".field").childNodes[x - 1].childNodes[y - 1].querySelector(".cell");
+    var f;
+    if (view_arr[x][y] === 0){
+        f = 1;
+        view_arr[x][y] = 1;
+    }
+    else {
+        f = 0;
+        aview_arr[x][y] = 0;
+    }
+    ///---////
 
+    if (f === 1){
+        content.innerHTML = "<i class='fas fa-flag'></i>";
+    }
+    else {
+        content.innerHTML = "";
+    }
+
+    }
 
